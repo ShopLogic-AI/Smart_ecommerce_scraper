261942d929109e8dd1add78fd446024a516df211
Salahjb <salah8jab@gmail.com>
2025-05-14 18:34:28 +0100

    delete the dummy files

--- utils/save_data.py
+++
@@ -1,8 +0,0 @@
-# utils/save_data.py
-import json
-import os
-
-def save_to_json(data, filename):
-    os.makedirs("data", exist_ok=True)
-    with open(f"data/{filename}", "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
--- utils/storage.py
+++
@@ -1,8 +0,0 @@
-# utils/storage.py
-import json
-import os
-
-def save_to_json(data, filename="data/output.json"):
-    os.makedirs("data", exist_ok=True)
-    with open(filename, "w") as f:
-        json.dump(data, f, indent=4)
